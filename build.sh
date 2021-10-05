fb27cdd91d3d1dbe09c531135d65f5c8801a474b
da <>
2021-10-05 16:17:20 +0200

    update build script

--- build.sh
+++ build.sh
@@ -1,39 +1,26 @@
 #!/bin/bash
 #
-# build for linux using clang++
+# build on linux w/ sdl2 as a backend
 
-# TODO use `sdl2-config --cflags`
-# TODO use `sdl2-config --libs`
-# TODO use -fno-gnu-unique for hotloading (?) (only gcc)
-# NOTE uncomment `bear -- \` line if you don't need a compile_commands.json
-
-# TODO CommonIncludes
-CommonFlags="-g -DIMGUI -Wall -Wfatal-errors -Wno-missing-braces -Wno-char-subscripts -fPIC"
+# NOTE comment out `bear -- \` line if you don't need a compile_commands.json
+CmnFlags="-g -DIMGUI -Wall -Wfatal-errors -Wno-missing-braces -Wno-char-subscripts -fPIC "
 # other flags that could be useful:
 # -Werror -Wno-comment -Wno-multichar -Wno-write-strings -Wno-unused-variable
 # -Wno-unused-function -Wno-sign-compare -Wno-unused-result
 # -Wno-strict-aliasing -Wno-int-to-pointer-cast -Wno-switch
 # -Wno-logical-not-parentheses -Wno-return-type -Wno-array-bounds -maes
 # -msse4.1 -std=c++11 -fno-rtti -fno-exceptions
+CmnFlags+=$(sdl2-config --cflags)
+CmnIncludes="-include ./src/base.h -I./src/ -I./dep/ -I./dep/imgui-1.82"
+CmnLibs="-L$(pwd)/dep -Wl,-rpath=$(pwd)/dep/ -limgui_sdl"
+SDL2Libs="$(sdl2-config --libs) -lSDL2_image -lSDL2_ttf "
 
 # build game as dll
 bear -- \
-clang++ ${CommonFlags} --shared \
-                     ./src/game.cpp \
-                     -include ./src/base.h \
-                     -o ./dep/libgame.so \
-                     -ldl -lpthread \
-                     -L`pwd`/dep \
-                     -Wl,-rpath=`pwd`/dep/ \
-                     -limgui_sdl \
-                     -I./src/ -I./dep/ -I./dep/imgui-1.82/
+clang++ ${CmnFlags} --shared ${CmnIncludes} ${CmnLibs} ./src/game.cpp -o ./dep/libgame.so
 
 # TODO compilation w/ gcc seems broken here (gets stuck)
 # build platform layer as executable
 bear --append -- \
-clang++ $CommonFlags \
-        ./src/platform_sdl.cpp -o ./bin/megastruct_platform \
-        -include ./src/base.h \
-        -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf -ldl -lpthread \
-        -L`pwd`/dep -Wl,-rpath=`pwd`/dep/ -limgui_sdl -ltmxlite \
-        -I./src/ -I./dep/ -I./dep/imgui-1.82/ -I/usr/include/SDL2
+clang++ ${CmnFlags} ${CmnIncludes} ${SDL2Libs} -ldl ${CmnLibs} -ltmxlite \
+        ./src/platform_sdl.cpp -o ./bin/megastruct
